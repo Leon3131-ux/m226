b032eceadee11c2735a659e9606db1cf1cb4e2bf
Turbo202 <michael.marchesi@edu.tbz.ch>
2020-10-30 11:34:13 +0100

    added necessary annotations

--- src/main/java/com/example/todo/domain/Task.java
+++ src/main/java/com/example/todo/domain/Task.java
@@ -1,11 +1,20 @@
 package com.example.todo.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import java.util.Date;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Task extends AbstractEntity{
 
     @Column(nullable = false)
