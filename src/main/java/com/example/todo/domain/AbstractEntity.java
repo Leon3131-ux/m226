db6f9ca94f0ae50df250b0b4d7e3670b7dc112b7
alessandropisani <Lama0303+>
2020-10-30 11:36:50 +0100

    test

--- src/main/java/com/example/todo/domain/AbstractEntity.java
+++ src/main/java/com/example/todo/domain/AbstractEntity.java
@@ -3,10 +3,10 @@ package com.example.todo.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.springframework.data.annotation.Id;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
 @AllArgsConstructor
