660b75849a7b29b5a28dc8b17225d0a2be98db32
alessandropisani <Lama0303+>
2020-10-31 15:42:54 +0100

    added task service

---
+++ src/main/java/com/example/todo/service/TaskService.java
@@ -0,0 +1,13 @@
+package com.example.todo.service;
+
+import com.example.todo.domain.Task;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+    public List<Task> getAllTasks(){
+
+    }
+}
