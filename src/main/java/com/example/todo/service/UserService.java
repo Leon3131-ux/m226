21761a7650f80f7677b18e18350dec4ea2d6f86b
leon3131-ux <56247087+Leon3131-ux@users.noreply.github.com>
2020-10-31 16:29:18 +0100

    added getCurrentUser method

--- src/main/java/com/example/todo/service/UserService.java
+++ src/main/java/com/example/todo/service/UserService.java
@@ -3,8 +3,11 @@ package com.example.todo.service;
 import com.example.todo.domain.User;
 import com.example.todo.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +17,10 @@ public class UserService {
 
     public User getUserOrThrowException(String username){
         return userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(username));
+    }
+
+    public Optional<User> getCurrentUser(){
+        return userRepository.findByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
     }
 
 }
