37a71f3ffbf1ae90fdc60b6e0106a0df8f5155a5
Turbo202 <michael.marchesi@edu.tbz.ch>
2020-11-01 14:00:52 +0100

    transfer

--- src/main/java/com/example/todo/validator/TaskValidator.java
+++ src/main/java/com/example/todo/validator/TaskValidator.java
@@ -1,12 +1,19 @@
 package com.example.todo.validator;
 
 import com.example.todo.domain.TaskDto;
+import com.example.todo.service.TaskService;
+import com.example.todo.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
 @Component
+@RequiredArgsConstructor
 public class TaskValidator implements Validator {
+
+    private final TaskService taskService;
+    private final UserService userService;
 
     @Override
     public boolean supports(Class<?> clazz) {
