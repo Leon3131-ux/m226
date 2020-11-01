e41e15914f6585a7977b8d077fc97ccde5effa06
leon3131-ux <56247087+Leon3131-ux@users.noreply.github.com>
2020-11-01 15:18:15 +0100

    bugfix

--- src/main/java/com/example/todo/service/UserDetailsServiceImp.java
+++ src/main/java/com/example/todo/service/UserDetailsServiceImp.java
@@ -6,9 +6,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Optional;
+
+@Service
 @RequiredArgsConstructor
 public class UserDetailsServiceImp implements UserDetailsService {
 
