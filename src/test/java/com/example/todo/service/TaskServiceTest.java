2506a9b19c5ddc98408584cc7215eefb95c914ec
leon3131-ux <56247087+Leon3131-ux@users.noreply.github.com>
2020-11-23 09:02:16 +0100

    refactored taskServiceTest

--- src/test/java/com/example/todo/service/TaskServiceTest.java
+++ src/test/java/com/example/todo/service/TaskServiceTest.java
@@ -5,6 +5,7 @@ import com.example.todo.domain.TaskDto;
 import com.example.todo.domain.User;
 import com.example.todo.repository.TaskRepository;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -27,18 +28,16 @@ public class TaskServiceTest {
     @Mock
     private TaskRepository taskRepository;
 
+    @BeforeEach
+    void initializeServiceMock(){
+        when(taskRepository.save(any(Task.class))).then(returnsFirstArg());
+    }
+
     @Test
     public void testTaskUpdate(){
         User user = new User("test", "test");
         Task originalTask = new Task("test", "test", false, new Date(), false, user);
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.YEAR, 1990);
-        calendar.set(Calendar.MONTH, Calendar.JANUARY);
-        calendar.set(Calendar.DAY_OF_MONTH, 1);
-        Date testDate = calendar.getTime();
-        TaskDto updateTask = new TaskDto(1L, "1", "1", true, testDate, true);
-
-        when(taskRepository.save(any(Task.class))).then(returnsFirstArg());
+        TaskDto updateTask = new TaskDto(1L, "1", "1", true, getDefaultDate(), true);
 
         taskService.updateTask(originalTask, updateTask);
 
@@ -48,6 +47,14 @@ public class TaskServiceTest {
         assertThat(originalTask.isDone()).isSameAs(updateTask.isDone());
         assertThat(originalTask.isDeleted()).isSameAs(updateTask.isDeleted());
         assertThat(originalTask.getUser()).isSameAs(user);
+    }
+
+    private Date getDefaultDate(){
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, 1990);
+        calendar.set(Calendar.MONTH, Calendar.JANUARY);
+        calendar.set(Calendar.DAY_OF_MONTH, 1);
+        return calendar.getTime();
     }
 
 }
