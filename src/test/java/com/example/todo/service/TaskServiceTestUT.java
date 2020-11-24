0d063a5e0a810480009daaacf682021ea2312fb0
leon3131-ux <56247087+Leon3131-ux@users.noreply.github.com>
2020-11-24 08:45:00 +0100

    refactoring

--- src/test/java/com/example/todo/service/TaskServiceTestUT.java
+++ src/test/java/com/example/todo/service/TaskServiceTestUT.java
@@ -20,7 +20,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
-public class TaskServiceTest {
+public class TaskServiceTestUT {
 
     @InjectMocks
     private TaskService taskService;
--- src/test/java/com/example/todo/validator/TaskValidatorTestUT.java
+++ src/test/java/com/example/todo/validator/TaskValidatorTestUT.java
@@ -24,7 +24,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
-public class TaskValidatorTest {
+public class TaskValidatorTestUT {
 
     @InjectMocks
     private TaskValidator taskValidator;
